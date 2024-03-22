12568bd3a1274be048600e7bf2efba57b77a6a52
JoaoPaulo-creator <joaosarto@gmail.com>
2024-03-22 00:02:17 -0300

    feat: add compiler structure

---
+++ compiler/compiler.go
@@ -0,0 +1,35 @@
+package compiler
+
+import (
+	"joaopaulo-creator/monkey-lang/ast"
+	"joaopaulo-creator/monkey-lang/code"
+	"joaopaulo-creator/monkey-lang/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) ByteCode() *ByteCode {
+	return &ByteCode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type ByteCode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
