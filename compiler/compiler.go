e604940f0e640abc1b429234757aa17d0ac93811
JoaoPaulo-creator <joaosarto@gmail.com>
2024-04-03 22:05:34 -0300

    feat: add Add operation in stack

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"joaopaulo-creator/monkey-lang/ast"
 	"joaopaulo-creator/monkey-lang/code"
 	"joaopaulo-creator/monkey-lang/object"
@@ -19,38 +20,45 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
-  switch node := node.(type){
-  case *ast.Program:
-    for _, s := range node.Statements{
-      err := c.Compile(s)
-      if err != nil {
-        return err
-      }
-    } 
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
 
-  case *ast.ExpressionStatement:
-    err := c.Compile(node.Expression)
-    if err != nil {
-      return err
-    }
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
 
-  case *ast.InfixExpression:
-    err := c.Compile(node.Left)
-    if err != nil {
-      return err
-    }
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
 
-    err = c.Compile(node.Right)
-    if err != nil {
-      return err
-    }
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
 
-  case *ast.IntegerLiteral: 
-    integer := &object.Integer{Value: node.Value}
-    c.emit(code.OpConstant, c.addConstant(integer))
-}
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
+		}
 
-  return nil
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
+	return nil
 }
 
 func (c *Compiler) ByteCode() *ByteCode {
@@ -66,22 +74,18 @@ type ByteCode struct {
 }
 
 func (c *Compiler) addConstant(obj object.Object) int {
-  c.constants = append(c.constants, obj)
-  return len(c.constants) -1 
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
 }
-
 
 func (c *Compiler) emit(op code.Opcode, operands ...int) int { // emit == generate
-  ins := code.Make(op, operands...)
-  pos := c.addInstructions(ins)
-  return pos 
+	ins := code.Make(op, operands...)
+	pos := c.addInstructions(ins)
+	return pos
 }
-
 
 func (c *Compiler) addInstructions(ins []byte) int {
-  posNewInstruction := len(c.instructions)
-  c.instructions = append(c.instructions, ins...)
-  return posNewInstruction
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
-
-
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -10,14 +10,12 @@ import (
 	"testing"
 )
 
-
 func parse(input string) *ast.Program {
-  l := lexer.New(input)
-  p := parser.New(l)
+	l := lexer.New(input)
+	p := parser.New(l)
 
-  return p.ParseProgram()
+	return p.ParseProgram()
 }
-
 
 func testIntegerObject(expected int64, actual object.Object) error {
 	result, ok := actual.(*object.Integer)
@@ -34,57 +32,52 @@ func testIntegerObject(expected int64, actual object.Object) error {
 	return nil
 }
 
-
 type vmTestCase struct {
-  input string
-  expected interface{}
+	input    string
+	expected interface{}
 }
 
 func runVmTests(t *testing.T, tests []vmTestCase) {
-  t.Helper()
+	t.Helper()
 
-  for _, tt := range tests {
-    program := parse(tt.input)
+	for _, tt := range tests {
+		program := parse(tt.input)
 
-    comp := compiler.New()
-    err := comp.Compile(program)
-    if err != nil {
-      t.Fatalf("compiler error: %s", err)
-    } 
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
 
-    vm := New(comp.ByteCode())
-    err = vm.Run()
-    if err != nil {
-      t.Fatalf("vm error: %s", err)
-    } 
+		vm := New(comp.ByteCode())
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
 
-    stackElem := vm.StackTop()
-  testExpectedObject(t, tt.expected, stackElem)    
-  }
+		stackElem := vm.StackTop()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
 }
 
-func testExpectedObject(t *testing.T, expected interface{}, actual object.Object){
-  t.Helper()
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
 
-  switch expected := expected.(type) { 
-  case int:
-    err := testIntegerObject(int64(expected), actual) 
-    if err != nil {
-      t.Errorf("testIntegerObject failed: %s", err)
-    }
-  }
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
 }
 
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 3},
+	}
 
-
-
-func TestIntegerArithmetic(t *testing.T) { 
-  tests := []vmTestCase{
-    {"1", 1},
-    {"2", 2},
-    {"1 + 2", 2}, // FIXME
-  }
-
-  runVmTests(t, tests)
+	runVmTests(t, tests)
 }
-
